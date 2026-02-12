ccb19a39639f98efa2962120a654167f6ad7243e
EastonPomrankey <pomkey9952@gmail.com>
2026-02-12 10:24:28 -0800

    working login and password reset

--- src/MealPlanner/MealPlanner/Controllers/AccountController.cs
+++ src/MealPlanner/MealPlanner/Controllers/AccountController.cs
@@ -36,7 +36,10 @@ namespace MealPlanner.Controllers;
                  return View(model);
             }
 
+            Console.WriteLine($"Attempting to log in user: {model.Email}, Password: {model.Password}");
+
            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+           Console.WriteLine(result.ToString());
 
             if (result.Succeeded)
             {
@@ -64,12 +67,15 @@ namespace MealPlanner.Controllers;
                 return View(model);
             }
 
+            Console.WriteLine($"Attempting to register user: {model.Email}, Password: {model.Password}");
+
             var user = new User
             {
                 FullName = model.Name,
                 UserName = model.Email,
                 NormalizedUserName = model.Email.ToUpper(),
                 Email = model.Email,
+                EmailConfirmed = true,
                 NormalizedEmail = model.Email.ToUpper(),
             };
 
@@ -124,6 +130,53 @@ namespace MealPlanner.Controllers;
                 return RedirectToAction("ChangePassword", "Account", new { username = user.UserName });
             }
 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ChangePassword(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("VerifyEmail", "Account");
+            }
+
+            return View(new ChangePasswordViewModel { Email = username });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Please correct the errors and try again.");
+                return View(model);
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "User not found");
+                return View(model);
+            }
+
+            var result = await _userManager.RemovePasswordAsync(user);
+
+            if (result.Succeeded)
+            {
+                result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
         }
 
         [HttpPost]
