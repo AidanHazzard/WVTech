4412ff6836c3adc489cf17f630426c23d85a10ec
EastonPomrankey <pomkey9952@gmail.com>
2026-02-14 15:11:34 -0800

    Initial working reset password feature

--- src/MealPlanner/MealPlanner/Controllers/HomeController.cs
+++ src/MealPlanner/MealPlanner/Controllers/HomeController.cs
@@ -4,22 +4,27 @@ using Microsoft.EntityFrameworkCore;
 using MealPlanner.ViewModels;
 using MealPlanner.Models;
 using Microsoft.AspNetCore.Authorization;
+using MealPlanner.Services;
+using System.Security.Claims;
 
 namespace MealPlanner.Controllers;
+
 public class HomeController : Controller
 {
-   private readonly MealPlannerDBContext _context;
+    private readonly MealPlannerDBContext _context;
+    private readonly IAccountService _accountService;
 
-public HomeController(MealPlannerDBContext context)
-{
-    _context = context;
-}
+    public HomeController(MealPlannerDBContext context, IAccountService accountService)
+    {
+        _context = context;
+        _accountService = accountService;
+    }
 
     public async Task<IActionResult> Index()
-        {
-           
-            return View();
-        }
+    {
+
+        return View();
+    }
 
     [Authorize]
     public IActionResult Privacy()
@@ -27,17 +32,19 @@ public HomeController(MealPlannerDBContext context)
         return View();
     }
 
-    public IActionResult Register()
-    {
-        return View();
-    }
+    //dont know why this is here
+    // public IActionResult Register()
+    // {
+    //     return View();
+    // }
 
+    //This is the specific admin view
     [Authorize(Roles = "Admin")]
     public IActionResult Admin()
     {
         return View();
     }
-
+    // This is the specific user view or dashboard
     [Authorize(Roles = "User")]
     public IActionResult User()
     {
