774673a5037cc639848ad2c23c9b4176df0f9457
Natalie Perez <nperez21@wou.edu>
2026-02-15 14:29:51 -0800

    added nutrition progress services and DTO records

--- src/MealPlanner/MealPlanner/Services/NutritionProgressDto.cs
+++ src/MealPlanner/MealPlanner/Services/NutritionProgressDto.cs
@@ -1,14 +1,5 @@
 namespace MealPlanner.Services;
 
-public interface INutritionProgressService
-{
-    Task<NutritionProgressDto> GetDailyProgressAsync(string userId, DateOnly day);
-
-    Task<NutritionProgressDto> GetRangeProgressAsync(string userId, DateOnly startDay, DateOnly endDay);
-}
-
-
-
 public record NutritionProgressDto(
     string UserId,
     DateOnly StartDay,
