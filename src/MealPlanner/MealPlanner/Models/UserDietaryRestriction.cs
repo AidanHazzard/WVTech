3974aa12e456884091b0af4447efd50e8f57d40c
nperez21 <152967211+nperez21@users.noreply.github.com>
2026-02-15 17:24:24 -0800

    Feature/dietary restrictions settings (#28)
    * Made change in MealPlannerDBContext.cs file, it now includes dietary restriction entities
    
    * In the UserDietaryRestriction.cs file, I added 'DietaryRestrictionId' to the join model in order to be able to store a certain restriction
    
    * I added two files, one in DAL/Abstract and another in DAL/Concrete. For DAL/Abstract I added an IUserDietaryRestrictionRepository interface
    
    * I added a dietary restriction service file under DAL for saving/retrieving the user's selections. I also included UserDietaryRestrictionRepository logic.
    
    * I added unit tests for dietary restictions behavior
    
    * I added UserDietaryRestrictionRepository in the dependency injection
    
    ---------
    
    Co-authored-by: Natalie Perez <nperez21@wou.edu>

--- src/MealPlanner/MealPlanner/Models/UserDietaryRestriction.cs
+++ src/MealPlanner/MealPlanner/Models/UserDietaryRestriction.cs
@@ -6,12 +6,18 @@ namespace MealPlanner.Models;
 [Table("UserDietaryRestriction")]
 public class UserDietaryRestriction
 {
+<<<<<<< HEAD
     [Key]
     public int Id { get; set; }
 
     public string UserId { get; set; }
     
     [ForeignKey("UserId")]
+=======
+    public int UserId { get; set; }
+>>>>>>> 2b2fd8c (In the UserDietaryRestriction.cs file, I added 'DietaryRestrictionId' to the join model in order to be able to store a certain restriction)
     public User User { get; set; }
+    public int DietaryRestrictionId { get; set; }
+    public DietaryRestriction DietaryRestriction { get; set; }
 
 }
