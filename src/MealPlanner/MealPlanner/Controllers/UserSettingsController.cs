711bc86f01759accc685c0d91ad58bf152fbc060
nperez21 <nperez21@mail.wou.edu>
2026-02-22 18:17:41 -0800

    I added DietarySettingsViewModel.cs file which is to help connect to UI and UserSettingsController.cs file to help manage dietary restrictions

---
+++ src/MealPlanner/MealPlanner/Controllers/UserSettingsController.cs
@@ -0,0 +1,83 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MealPlanner.Models;
+using MealPlanner.ViewModels;
+
+namespace MealPlanner.Controllers
+{
+    [Authorize]
+    public class UserSettingsController : Controller
+    {
+        private readonly MealPlannerDBContext _db;
+
+        public UserSettingsController(MealPlannerDBContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Dietary()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var allRestrictions = await _db.DietaryRestrictions
+                .OrderBy(d => d.Name)
+                .ToListAsync();
+
+            var selectedIds = await _db.UserDietaryRestrictions
+                .Where(x => x.UserId == userId)
+                .Select(x => x.DietaryRestrictionId)
+                .ToListAsync();
+
+            var vm = new DietarySettingsViewModel
+            {
+                Restrictions = allRestrictions.Select(r => new DietaryRestrictionOptionViewModel
+                {
+                    DietaryRestrictionId = r.DietaryRestrictionId,
+                    Name = r.Name,
+                    IsSelected = selectedIds.Contains(r.DietaryRestrictionId)
+                }).ToList()
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Dietary(DietarySettingsViewModel vm)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var chosen = vm.Restrictions
+                .Where(x => x.IsSelected)
+                .Select(x => x.DietaryRestrictionId)
+                .ToHashSet();
+
+            var existing = await _db.UserDietaryRestrictions
+                .Where(x => x.UserId == userId)
+                .ToListAsync();
+
+            _db.UserDietaryRestrictions.RemoveRange(existing);
+
+            var newRows = chosen.Select(id => new UserDietaryRestriction
+            {
+                UserId = userId,
+                DietaryRestrictionId = id
+            });
+
+            await _db.UserDietaryRestrictions.AddRangeAsync(newRows);
+            await _db.SaveChangesAsync();
+
+            TempData["Message"] = "Dietary restrictions saved.";
+            return RedirectToAction(nameof(Dietary));
+        }
+    }
+}
