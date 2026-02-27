88595c3ad6372dc31ead8d4f60ed8b0d3f11541c
Cooperdteer <cooperdteer@gmail.com>
2026-02-27 02:30:17 -0800

    Finished scroll bar and started adding checkbox

--- src/MealPlanner/MealPlanner/wwwroot/js/indexPlanner.js
+++ src/MealPlanner/MealPlanner/wwwroot/js/indexPlanner.js
@@ -3,12 +3,75 @@ document.addEventListener('DOMContentLoaded', function () {
     const rightArrow = document.getElementById('ARight');
     const container = document.getElementById('back1DatesInnerContainer');
 
-    let scrollInterval;
+    const dateText = document.getElementById('modelsDate').textContent;
+    const curDate = new Date(dateText);
 
+    for (i = 400; i >= 1; i--) {
+        let nextDate = new Date(curDate);
+        nextDate.setDate(curDate.getDate() - i);
+        MakeDate(nextDate);
+        if (i === 4) {
+            tail = nextDate;
+        }
+    }
+
+    MakeSelected();
+
+    for (i = 1; i < 400; i++) {
+        let nextDate = new Date(curDate);
+        nextDate.setDate(curDate.getDate() + i);
+        MakeDate(nextDate);
+    }
+
+    document.getElementById('Selected').scrollIntoView({ behavior: 'instant', block: 'nearest', inline: 'center' });
+
+    function MakeDate(nextDate) {
+        nextDate = new Date(nextDate);
+        const shortMonth = nextDate.toLocaleString('en-US', { month: 'short' });
+
+        const newDate = document.createElement('form');
+        newDate.action = '/Home/Index';
+        newDate.method = 'get';
+
+        const dateButton = document.createElement('button');
+        dateButton.className = 'Date';
+        dateButton.name = 'date';
+        dateButton.value = nextDate.toISOString();
+
+        const buttonsText = document.createElement('h2');
+        buttonsText.className = 'back1Title';
+        buttonsText.innerHTML = `${shortMonth}<br />${nextDate.getDate().toString()}`;
+
+        dateButton.append(buttonsText);
+        newDate.append(dateButton);
+        container.append(newDate);
+    }
+
+    function MakeSelected() {
+        const shortMonth = curDate.toLocaleString('en-US', { month: 'short' });
+
+        const newDate = document.createElement('form');
+        newDate.action = '/Home/Index';
+        newDate.method = 'get';
+
+        const dateDiv = document.createElement('div');
+        dateDiv.id = 'Selected';
+
+        const divText = document.createElement('h2');
+        divText.className = 'back1Title';
+        divText.innerHTML = `${shortMonth}<br />${curDate.getDate().toString()}`;
+
+        dateDiv.append(divText);
+        newDate.append(dateDiv);
+        container.append(newDate);
+    }
+
+    let scrollInterval;
+    
     function startScroll(direction) {
         scrollInterval = setInterval(() => {
-            container.scrollBy({ left: direction * 5, behavior: 'auto' });
-        }, 10);
+            container.scrollBy({ left: direction * 10, behavior: 'auto' });
+        }, 5);
     }
 
     function stopScroll() {
