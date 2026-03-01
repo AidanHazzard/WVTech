21d0993ea00f02688d832cb08e52f7bfcd8916de
nperez21 <nperez21@mail.wou.edu>
2026-03-01 03:12:52 -0800

    implementing public landing and authenticated dashboard routing

--- src/MealPlanner/MealPlanner/Controllers/LoginController.cs
+++ src/MealPlanner/MealPlanner/Controllers/LoginController.cs
@@ -24,7 +24,7 @@ public class LoginController : Controller
 
         var result = await _loginService.LoginUserAsync(model);
 
-        if (result.Succeeded) return RedirectToAction("Index", "Home");
+        if (result.Succeeded) return RedirectToAction("Dashboard", "Home");
 
         ModelState.AddModelError("", "Invalid login attempt.");
         return View(model);
