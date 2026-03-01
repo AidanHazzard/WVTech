d99599585ec6564b3a91126ac0dbc791ebf1ee25
nperez21 <nperez21@mail.wou.edu>
2026-03-01 02:46:00 -0800

    added favorites/my favorite recipes page

--- src/MealPlanner/MealPlanner/wwwroot/js/recipeSearchToView.js
+++ src/MealPlanner/MealPlanner/wwwroot/js/recipeSearchToView.js
@@ -1,11 +1,12 @@
 $(document).ready(() =>
-    {
-        $(document).on("click", "#recipeSearchRow", goToView);
-    }
-);
-
+{
+    $(document).on("click", "#recipeSearchRow", goToView);
+});
+    
 function goToView(event)
 {
+    if ($(event.target).closest(".favoriteForm").length > 0) return;
+    
     const id = $("#recipeId", this).text();
     location.href = `/FoodEntries/Recipes/${id}`;
 }
