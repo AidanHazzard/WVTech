0fdbd2b59baea9b9351dca37e99c255889ecf0c8
nperez21 <nperez21@mail.wou.edu>
2026-02-28 22:32:17 -0800

    updated search result navigation

--- src/MealPlanner/MealPlanner/wwwroot/js/recipeSearchToView.js
+++ src/MealPlanner/MealPlanner/wwwroot/js/recipeSearchToView.js
@@ -1,11 +1,8 @@
-$(document).ready(() =>
-    {
-        $(document).on("click", "#recipeSearchRow", goToView);
-    }
-);
+$(document).ready(() => {
+    $(document).on("click", ".recipeSearchRow", goToView);
+});
 
-function goToView(event)
-{
-    const id = $("#recipeId", this).text();
+function goToView(event) {
+    const id = $(this).find(".recipeId").text().trim();
     location.href = `/FoodEntries/Recipes/${id}`;
 }
