60503a49e13e2385efc089a1c3061d05c92c2e45
EastonPomrankey <pomkey9952@gmail.com>
2026-03-16 14:01:08 -0700

    refactoring reset pw from account

--- src/MealPlanner/MealPlanner/Controllers/AccountSettingsController.cs
+++ src/MealPlanner/MealPlanner/Controllers/AccountSettingsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MealPlanner.Controllers
 {
-    [Authorize(Roles = "User")]
+    [Authorize]
     public class AccountSettingsController : Controller
     {
         private readonly IAccountSettingsService _accountSettingsService;
